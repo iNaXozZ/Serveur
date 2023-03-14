5b2cd9617cf82b9ef9ca28495025c6295ed1e34f
lher-emilie <emilie.lher@etudiant.univ-brest.fr>
2023-03-14 12:18:02 +0100

    rmdir version 1
    rmdrir

--- CommandeRMDIR.java
+++ CommandeRMDIR.java
@@ -1,4 +1,7 @@
+import java.io.File;
+
 import java.io.PrintStream;
+
 
 public class CommandeRMDIR extends Commande {
 	
@@ -8,8 +11,20 @@ public class CommandeRMDIR extends Commande {
 	
 	public void execute() {
 		
-		
-		
-	}
+		File file = new File(repertoireCourant+"\\"+ commandeArgs[0]);
+		        File[] liste = file.listFiles();
+		          for(File item : liste){
+		            if(item.isDirectory()== true)
+		            { 
+		            	file.delete();
+		            	
+		              ps.println("fichier supprimer"); 
+		            } 
+		            else 
+		            {
+		              ps.println("pas supprimer"); 
+		            } 
 
+		          }
+		    }
 }
