d95507bf842987320d764972c76d77520b48073e
lher-emilie <emilie.lher@etudiant.univ-brest.fr>
2023-03-14 11:06:43 +0100

    Update CommandeLS.java
    version 1.1 ls

--- CommandeLS.java
+++ CommandeLS.java
@@ -22,19 +22,21 @@ public class CommandeLS extends Commande {
 		File file= new File(".");
 		File[] liste=file.listFiles();
 		
-		//chemin en dur
-		//String s = file.getAbsoluteFile().toString();
 		
 		//lire les fichiers d'un dossier en utilisant la maéthode newDIrectoryStream
 		for(File item : liste){
+			if(file.getAbsoluteFile()!=null) {
 	        if(item.isFile())
 	        { 
+	        	
 	          System.out.format("Nom du fichier: %s%n", item.getName()); 
 	        } 
 	        else if(item.isDirectory())
 	        {
+	        	
 	          System.out.format("Nom du répertoir: %s%n", item.getName()); 
 	        }
+			}
 		}
 	
 				
