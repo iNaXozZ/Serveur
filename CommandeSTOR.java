7a5f33eb5a548007250b5aec6d8d6eed9c3522f8
RTacon <romain.tacon.coc@gmail.com>
2023-03-15 19:15:26 +0100

    Commande STOR OK
    ok

--- CommandeSTOR.java
+++ CommandeSTOR.java
@@ -1,6 +1,7 @@
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -22,31 +23,41 @@ public class CommandeSTOR extends Commande {
 			//Création du flux pour envoyer :
 			out = new PrintStream(transfertSocket.getOutputStream());
 			
-			File fichier = new File(repertoireCourant + commandeArgs[0]);
+			//Récupération du chemin absolu du répertoire où l'on veut créer le fichier
+			File fichierClient = new File(repertoireCourant + commandeArgs[0]);
 			File fichierServeur = new File("");
 			String fichierString = fichierServeur.getAbsolutePath()+"\\";
+			//Récupération du chemin absolu du fichier recherché dans le serveur 
+			File fichierDuServeur = new File(fichierString+ commandeArgs[0]);
 			
-			InputStream is = new FileInputStream(fichierString);
-			
+			// Lecture des flux d'écriture et de lecture
+			InputStream is = new FileInputStream(fichierClient);
 		    BufferedInputStream bis = new BufferedInputStream(is);
-		    FileOutputStream fos = new FileOutputStream(fichier);
+		    FileOutputStream fos = new FileOutputStream(fichierDuServeur);
 		    
-		    //Création du buffer
-		    byte[] buffer = new byte[(int)fichier.length()];
-            int compteur;
-            ps.println("test");
-            // Écriture du contenu du fichier dans un fichier local
-            while ((compteur = bis.read(buffer)) != -1) {
-                fos.write(buffer, 0, compteur);
-            }
-            ps.println("Transfert réussi");
-            
+		    //Vérification que l'argument passé en paramètre dans la commande est bien un nom de fichier qui existe dans le répertoire du serveur 
+		    if(fichierClient.isFile())
+			{
+		    	//Création du buffer
+			    byte[] buffer = new byte[(int)fichierClient.length()];
+	            int compteur;
+	            // Écriture du contenu du fichier dans le fichier local du client
+	            while ((compteur = bis.read(buffer)) != -1) {
+	                fos.write(buffer, 0, compteur);
+	                out.println(buffer);
+	            }
+				
+			}
+		    ps.println("0 Commande GET OK");
             transfertSocket.close();
+            out.close();
             bis.close();
             fos.close();
 
+			} catch (FileNotFoundException te) {
+				ps.println("2 impossible de trouver le fichier "+te);
 			} catch (Exception e) {
-				ps.println("2 Problème lors de l'éxécution de la commande STOR");
+				ps.println("2 Problème lors de l'éxécution de la commande GET "+e);
 			}
 	}
 
